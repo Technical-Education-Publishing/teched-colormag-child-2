94e80f366811967d3a50592f65edb1ccbbd84cee
Eric Defore <eric.defore@gmail.com>
2019-03-19 12:19:00 -0400

    Remove dependency on the "Child Theme Configurator" plugin. Also brings in my usual build systems

--- functions.php
+++ functions.php
@@ -1,22 +1,76 @@
 <?php
+/**
+ * The theme's functions file that loads on EVERY page, used for uniform functionality.
+ *
+ * @since   {{VERSION}}
+ * @package Colormag_Child_2
+ */
+
 // Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) exit;
 
-// BEGIN ENQUEUE PARENT ACTION
-// AUTO GENERATED - Do not modify or remove comment markers above or below:
+// Make sure PHP version is correct
+if ( ! version_compare( PHP_VERSION, '5.3.0', '>=' ) ) {
+	wp_die( _x( 'ERROR in ColorMag Child 02 theme: PHP version 5.3 or greater is required.', 'PHP Version too low Error', 'colormag-child-2' ) );
+}
 
-if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
-    function chld_thm_cfg_parent_css() {
-        wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array(  ) );
-    }
-endif;
-add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
-         
-if ( !function_exists( 'child_theme_configurator_css' ) ):
-    function child_theme_configurator_css() {
-        wp_enqueue_style( 'chld_thm_cfg_separate', trailingslashit( get_stylesheet_directory_uri() ) . 'ctc-style.css', array( 'chld_thm_cfg_parent','colormag_style','colormag-fontawesome' ) );
-    }
-endif;
-add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css' );
+// Make sure no theme constants are already defined (realistically, there should be no conflicts)
+if ( defined( 'THEME_VER' ) ||
+	defined( 'THEME_URL' ) ||
+	defined( 'THEME_DIR' ) ||
+	defined( 'THEME_FILE' ) ) {
+	wp_die( _x( 'ERROR in ColorMag Child 02 theme: There is a conflicting constant. Please either find the conflict or rename the constant.', 'Constant or Global already in use Error', 'colormag-child-2' ) );
+}
 
-// END ENQUEUE PARENT ACTION
+/**
+ * Define Constants based on our Stylesheet Header. Update things only once!
+ */
+$theme_header = wp_get_theme();
+
+define( 'THEME_VER', $theme_header->get( 'Version' ) );
+define( 'THEME_NAME', $theme_header->get( 'Name' ) );
+define( 'THEME_URL', get_stylesheet_directory_uri() );
+define( 'THEME_DIR', get_stylesheet_directory() );
+
+$parent_theme = wp_get_theme( $theme_header->get( 'Template' ) );
+
+// Parent Theme didn't have any cache busting. This will at least do so for the main styles
+define( 'PARENT_THEME_VER', $parent_theme->get( 'Version' ) );
+
+add_action( 'init', 'teched_register_scripts' );
+function teched_register_scripts() {
+	
+	wp_register_script(
+		'teched',
+		THEME_URL . '/dist/assets/js/app.js',
+		array( 'jquery' ),
+		defined( 'WP_DEBUG' ) && WP_DEBUG ? time() : THEME_VER,
+		true
+	);
+	
+	wp_register_style(
+		'teched-parent',
+		get_template_directory_uri() . '/style.css',
+		array(),
+		defined( 'WP_DEBUG' ) && WP_DEBUG ? time() : PARENT_THEME_VER,
+		'all'
+	);
+	
+	wp_register_style(
+		'teched',
+		THEME_URL . '/dist/assets/css/app.css',
+		array( 'teched-parent' ),
+		defined( 'WP_DEBUG' ) && WP_DEBUG ? time() : THEME_VER,
+		'all'
+	);
+	
+}
+
+add_action( 'wp_enqueue_scripts', 'teched_enqueue_scripts' );
+function teched_enqueue_scripts() {
+	
+	//wp_enqueue_script( 'teched' );
+	
+	wp_enqueue_style( 'teched' );
+	
+}
